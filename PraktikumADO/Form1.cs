73ad1118f1a626dede8ad6acef8f374549123dbf
anneisza <rizaanneisza@gmail.com>
2026-03-13 14:21:12 +0700

    Menambahkan try catch untuk melengkapi method tombol hitung Mahasiswa

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -51,7 +51,19 @@ namespace PraktikumADO
         //Praktikum 2 : Tombol Hitung Mahasiswa
         private void btnHitungMhs_Click(object sender, EventArgs e)
         {
-
+            try{
+                Koneksi();
+                conn.Open();
+                string query = "SELECT COUNT(*) FROM Mahasiswa";
+                cmd = new SqlCommand(query, conn);
+                int jumlah = (int)cmd.ExecuteScalar();
+                txtHasil.Text = jumlah.ToString();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); 
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
