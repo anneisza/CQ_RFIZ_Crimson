a18b738407c0a05efdef6ed309664c2cd0f6f2a9
anneisza <rizaanneisza@gmail.com>
2026-03-13 14:30:06 +0700

    Membuat method untuk tombol Update Data

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -84,6 +84,25 @@ namespace PraktikumADO
                 MessageBox.Show(ex.Message);
             }
         }
+
+        //PraktikumADO 4 : Update Data (ExecuteNonQuery)
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+                string query = "UPDATE Mahasiswa SET Alamat = 'Yogyakarta' WHERE NIM = '23110100001'";
+                cmd = new SqlCommand(query, conn);
+                int hasil = cmd.ExecuteNonQuery();
+                MessageBox.Show("Jumlah baris terpengaruh: " + hasil);
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
         private void label1_Click(object sender, EventArgs e)
         {
 
