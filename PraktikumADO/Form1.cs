64cb7b3d3608dbc053cc15ae7e3379a0114281d7
anneisza <rizaanneisza@gmail.com>
2026-03-13 14:02:58 +0700

    Membuka koneksi database

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -29,6 +29,23 @@ namespace PraktikumADO
                 "Data Source=RIZA\\RIZAFI;Initial Catalog=PraktikumADO;Integrated Security=True"
                 );
         }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                MessageBox.Show("Koneksi ke database berhasil");
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
         private void label1_Click(object sender, EventArgs e)
         {
 
